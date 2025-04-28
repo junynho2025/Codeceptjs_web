2af0453c7979cfe806aed13a5ddae78f538c5119
junynho2011 <junynho2011@gmail.com>
2025-04-28 18:57:41 -0300

    Finalizando os testes

--- login_test.js
+++ login_test.js
@@ -16,16 +16,28 @@ Scenario('login com sucesso',  ({ I }) => {
 Scenario('logar apenas com email',  ({ I }) => {
 
     I.amOnPage('http://automationpratice.com.br')
+    I.click('Login')
+    I.fillField('#user','junynho2011@gmail.com')
+    I.click('#btnLogin')
+    I.waitForText('Senha inválida.', 3)
 
-})
+}).tag('@semsenha')
 
 Scenario('logar sem senha e sem email',  ({ I }) => {
 
     I.amOnPage('http://automationpratice.com.br')
+    I.click('Login')
+    I.fillField('#password', secret('123456'))
+    I.click('#btnLogin')
+    I.waitForText('E-mail inválido..', 3)
 
-})
+}).tag('@sememail')
+
 Scenario('logar apenas com senha',  ({ I }) => {
 
     I.amOnPage('http://automationpratice.com.br')
+    I.click('Login')
+    I.click('#btnLogin')
+    I.waitForText('E-mail inválido..', 3)
 
-})
+}).tag('@semdados')
